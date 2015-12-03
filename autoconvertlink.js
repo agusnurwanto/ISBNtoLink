47075ccb6403f7da8d7a22e328c20a7dd7575fe7
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2015-12-03 08:59:30 +0700

    add facebook in exclude_matches list

--- autoconvertlink.js
+++ autoconvertlink.js
@@ -7,13 +7,13 @@ function autoConvertLink(n){
 			if(childs[j].nodeType == Node.TEXT_NODE 
 				&& childs[j].nodeValue.trim() != '' 
 				&& childs[j].parentElement.getAttribute('class') != 'autoConvertLink'){
-				var text = 	childs[j].nodeValue.match(/978[0-9]{10}/g);
+				var text = 	childs[j].nodeValue.match(/978(-?)[0-9]{10}/g);
 				if(text){
 					var k = 0;
-					childs[j].parentElement.innerHTML = childs[j].nodeValue.replace(/978[0-9]{10}/g,function() {
-						// var url = 'http://bookscouter.com/prices.php?isbn='+text[k]+'&searchbutton=Sell';
-						var url = 'https://www.bookbyte.com/buyback2.aspx?isbns='+text[k];
-						setPopover(text[k]+''+k, url);
+					childs[j].parentElement.innerHTML = childs[j].nodeValue.replace(/978(-?)[0-9]{10}/g,function() {
+						var urlPrice = 'https://www.bookbyte.com/buyback2.aspx?isbns='+text[k];
+						setPopover(text[k]+''+k, urlPrice);
+						var url = 'http://bookscouter.com/prices.php?isbn='+text[k]+'&searchbutton=Sell';
 				        return '<a href="'+url+'" id="'+text[k]+''+k+'" target="blank" class="autoConvertLink" style="color:red; text-decoration:underline">'
 				        	+text[k++]
 				        	+'</a>';
@@ -52,4 +52,31 @@ function setPopover(id, url){
     	setTimeout(function(){ setPopover(id) }, 5000);
     }
 }
+
+// set library manual, without manifest.json
+function setLibrary(){
+	var bootsrapCSS = chrome.extension.getURL("bootsrap.min.css");
+	var customCSS = chrome.extension.getURL("custom.css");
+	var bootsrapJS = chrome.extension.getURL("bootsrap.min.js");
+	var jquery = chrome.extension.getURL("jquery.min.js");
+
+	(function() {
+	    var css = document.createElement('link'); css.rel = 'stylesheet';
+	    css.src = bootsrapCSS;
+	    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('head')[0]).appendChild(css);
+
+	    var css = document.createElement('link'); css.rel = 'stylesheet';
+	    css.src = customCSS;
+	    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('head')[0]).appendChild(css);
+	    
+	    var js = document.createElement('script'); js.type = 'text/javascript'; js.async = true;
+	    js.src = jquery;
+	    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('head')[0]).appendChild(js);
+	    
+	    var bootstrap = document.createElement('script'); bootstrap.type = 'text/javascript'; bootstrap.async = true;
+	    bootstrap.src = bootsrapJS;
+	    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('head')[0]).appendChild(bootstrap);
+	})();
+}
+
 autoConvertLink(0);
